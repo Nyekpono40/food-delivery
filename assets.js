67b18aa90acd3dfc3ee024feaa36529833549db0
Nyekpono40 <okoibuakaninyene@gmail.com>
2025-07-25 15:45:23 +0100

    first commit

---
+++ assets.js
@@ -0,0 +1,390 @@
+import food_logo from './food_logo.jpg'
+import search from './search.svg'
+import shopping_basket from './shopping_basket.svg'
+import food_background from './food_background.jpg'
+import add_icon from './add_icon.svg'
+import add_icon_green from './add_icon_green.svg'
+import close_icon_black from './close_icon_black.svg'
+import close_icon_green from './close_icon_green.svg'
+import rating from './rating.png'
+import facebook from './facebook.png'
+import twitter from './twitter.png'
+import linkedin from './linkedin.png'
+import appstore from './appstore.png'
+import playstore from './playstore.png'
+import app_store from './app_store.png'
+import google_play from './google_play.png'
+import apple from './apple.png'
+import clos from './clos.svg'
+
+
+import menu_1 from './menu_1.jpg'
+import menu_2 from './menu_2.jpg'
+import menu_3 from './menu_3.jpg'
+import menu_4 from './menu_4.jpg'
+import menu_5 from './menu_5.jpg'
+import menu_6 from './menu_6.jpg'
+import menu_7 from './menu_7.jpg'
+import menu_8 from './menu_8.jpg'
+
+
+
+
+import food_1 from './food_1.jpg'
+import food_2 from './food_2.jpg'
+import food_3 from './food_3.jpg'
+import food_4 from './food_4.jpg'
+import food_5 from './food_5.jpg'
+import food_6 from './food_6.jpg'
+import food_7 from './food_7.jpg'
+import food_8 from './food_8.jpg'
+import food_9 from './food_9.jpg'
+import food_10 from './food_10.jpg'
+import food_11 from './food_11.jpg'
+import food_12 from './food_12.jpg'
+import food_13 from './food_13.jpg'
+import food_14 from './food_14.jpg'
+import food_15 from './food_15.jpg'
+import food_16 from './food_16.jpg'
+import food_17 from './food_17.jpg'
+import food_18 from './food_18.jpg'
+import food_19 from './food_19.jpg'
+import food_20 from './food_20.jpg'
+import food_21 from './food_21.jpg'
+import food_22 from './food_22.jpg'
+import food_23 from './food_23.jpg'
+import food_24 from './food_24.jpg'
+import food_25 from './food_25.jpg'
+import food_26 from './food_26.jpg'
+import food_27 from './food_27.jpg'
+import food_28 from './food_28.jpg'
+import food_29 from './food_29.jpg'
+import food_30 from './food_30.jpg'
+import food_31 from './food_31.jpg'
+import food_32 from './food_32.jpg'
+
+
+export const assets ={
+    food_logo,
+    search,
+    shopping_basket,
+    food_background,
+    rating,
+    add_icon,
+    close_icon_black,
+    close_icon_green,
+    add_icon_green,
+    facebook,
+    linkedin,
+    twitter,
+    playstore,
+    appstore,
+    app_store,
+    google_play,
+    apple,
+    clos
+    
+
+
+
+
+
+
+}
+
+export const menu_list =[
+    {
+        menu_name:'Rice',
+        menu_image:menu_1
+    },
+    {
+        menu_name:'Pasta',
+        menu_image:menu_2
+    },
+    {
+        menu_name:'Salad',
+        menu_image:menu_3
+    },
+    {
+        menu_name:'Sandwich',
+        menu_image:menu_4
+    },
+    {
+        menu_name:'Deserts',
+        menu_image:menu_5
+    },
+    {
+        menu_name:'Rolls',
+        menu_image:menu_6
+    },
+    {
+        menu_name:'Cake',
+        menu_image:menu_7
+    },
+    {
+        menu_name:'Noodles',
+        menu_image:menu_8
+    }
+
+]
+
+ export const food_list =[
+    {
+        id:"1",
+        name:"Coconut rice",
+        image:food_1,
+        price:12,
+        description:'Good food provides immunity against diseases',
+        category:'Rice'
+    },
+    {
+        id:"2",
+        name:"Rice and stew",
+        image:food_2,
+        price:18,
+        description:'Good food provides immunity against diseases',
+        category:'Rice'
+    },
+    {
+        id:"3",
+        name:"Rice with Salad",
+        image:food_3,
+        price:16,
+        description:'Good food provides immunity against diseases',
+        category:'Rice'
+    },
+    {
+        id:"4",
+        name:"Fried rice",
+        image:food_4,
+        price:11,
+        description:'Good food provides immunity against diseases',
+        category:'Rice'
+    },
+     {
+        id:"5",
+        name:"veg pasta",
+        image:food_5,
+        price:10,
+        description:'Good food provides immunity against diseases',
+        category:'Pasta'
+    },
+    {
+        id:"6",
+        name:"chicken pasta",
+        image:food_6,
+        price:14,
+        description:'Good food provides immunity against diseases',
+        category:'Pasta'
+    },
+    {
+        id:"7",
+        name:"sauce pasta",
+        image:food_7,
+        price:20,
+        description:'Good food provides immunity against diseases',
+        category:'Pasta'
+    },
+    {
+        id:"8",
+        name:"creamy pasta",
+        image:food_8,
+        price:18,
+        description:'Good food provides immunity against diseases',
+        category:'Pasta'
+    },
+    {
+        id:"9",
+        name:"veg salad",
+        image:food_9,
+        price:10,
+        description:'Good food provides immunity against diseases',
+        category:'Salad'
+    },
+    {
+        id:"10",
+        name:"Italian salad",
+        image:food_10,
+        price:14,
+        description:'Good food provides immunity against diseases',
+        category:'Salad'
+    },
+    {
+        id:"11",
+        name:"chicken salad",
+        image:food_11,
+        price:20,
+        description:'Good food provides immunity against diseases',
+        category:'Salad'
+    },
+    {
+        id:"12",
+        name:"clover salad",
+        image:food_12,
+        price:22,
+        description:'Good food provides immunity against diseases',
+        category:'Salad'
+    },
+    {
+        id:"13",
+        name:"bread sandwich",
+        image:food_13,
+        price:20,
+        description:'Good food provides immunity against diseases',
+        category:'Sandwich'
+    },
+    {
+        id:"14",
+        name:"chicken sandwich",
+        image:food_14,
+        price:20,
+        description:'Good food provides immunity against diseases',
+        category:'Sandwich'
+    },
+    {
+        id:"15",
+        name:"grilled sandwich",
+        image:food_15,
+        price:20,
+        description:'Good food provides immunity against diseases',
+        category:'Sandwich'
+    },
+    {
+        id:"16",
+        name:"vegan sandwich",
+        image:food_16,
+        price:20,
+        description:'Good food provides immunity against diseases',
+        category:'Sandwich'
+    },
+    {
+        id:"17",
+        name:"vanilla ice cream",
+        image:food_17,
+        price:20,
+        description:'Good food provides immunity against diseases',
+        category:'Deserts'
+    },
+    {
+        id:"18",
+        name:"ripple ice cream",
+        image:food_18,
+        price:22,
+        description:'Good food provides immunity against diseases',
+        category:'Deserts'
+    },
+    {
+        id:"19",
+        name:"jar ice cream",
+        image:food_19,
+        price:20,
+        description:'Good food provides immunity against diseases',
+        category:'Deserts'
+    },
+    {
+        id:"20",
+        name:"fruit ice cream",
+        image:food_20,
+        price:25,
+        description:'Good food provides immunity against diseases',
+        category:'Deserts'
+    },
+    {
+        id:"21",
+        name:"peri-peri rolls",
+        image:food_21,
+        price:20,
+        description:'Good food provides immunity against diseases',
+        category:'Rolls'
+    },
+    {
+        id:"22",
+        name:"veg rolls",
+        image:food_22,
+        price:24,
+        description:'Good food provides immunity against diseases',
+        category:'Rolls'
+    },
+    {
+        id:"23",
+        name:"lasagna rolls",
+        image:food_23,
+        price:19,
+        description:'Good food provides immunity against diseases',
+        category:'Rolls'
+    },
+    {
+        id:"24",
+        name:"Chicken rolls",
+        image:food_24,
+        price:23,
+        description:'Good food provides immunity against diseases',
+        category:'Rolls'
+    },
+    {
+        id:"25",
+        name:"sliced cake",
+        image:food_25,
+        price:20,
+        description:'Good food provides immunity against diseases',
+        category:'Cake'
+    },
+    {
+        id:"26",
+        name:"butterscotch cake",
+        image:food_26,
+        price:18,
+        description:'Good food provides immunity against diseases',
+        category:'Cake'
+    },
+    {
+        id:"27",
+        name:"vegan cake",
+        image:food_27,
+        price:23,
+        description:'Good food provides immunity against diseases',
+        category:'Cake'
+    },
+    {
+        id:"28",
+        name:"cupcake",
+        image:food_28,
+        price:15,
+        description:'Good food provides immunity against diseases',
+        category:'Cake'
+    },
+    {
+        id:"29",
+        name:"butter noodles",
+        image:food_29,
+        price:19,
+        description:'Good food provides immunity against diseases',
+        category:'Noodles'
+    },
+    {
+        id:"30",
+        name:"cooked noodles",
+        image:food_30,
+        price:15,
+        description:'Good food provides immunity against diseases',
+        category:'Noodles'
+    },
+    {
+        id:"31",
+        name:"somen noodles",
+        image:food_31,
+        price:22,
+        description:'Good food provides immunity against diseases',
+        category:'Noodles'
+    },
+    {
+        id:"32",
+        name:"veg noodles",
+        image:food_32,
+        price:24,
+        description:'Good food provides immunity against diseases',
+        category:'Noodles'
+    },
+]
+
+
